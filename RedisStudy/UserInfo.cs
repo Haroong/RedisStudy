e907a98678f88e9efbbf57a41ef1b36118524963
조혜륜 <absolutecool18@gmail.com>
2023-02-10 16:01:09 +0900

    초기 커밋.

---
+++ RedisStudy/UserInfo.cs
@@ -0,0 +1,9 @@
+namespace RedisStudy
+{
+    public class UserInfo
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public uint age { get; set; }
+    }
+}
